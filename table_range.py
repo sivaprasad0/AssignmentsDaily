3f42468ca85a32bddb724054eb68fc4dfe833576
Durga Siva prasad Rajabatthula <67005488+sivaprasad0@users.noreply.github.com>
2021-08-13 19:33:44 +0530

    Delete table_range.py

--- table_range.py
+++
@@ -1,7 +0,0 @@
-no = int(input("enter number for which table is needed: "))
-
-range_start=int(input("enter range start"))
-range_end=int(input("enter range end"))
-
-for i in range(range_start,range_end+1):
-	print(no," * ",i," = ",no*i)
